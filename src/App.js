007f1926fe9d7445cc086a650ee5cf18945c8bc1
Vanesa Paola Soria <vanesasoria.com@gmail.com>
2022-04-25 19:05:41 -0300

    mis cambios

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@
 import './App.css';
 import React, { useState, createContext } from 'react';
 import  NavBar  from './components/NavBar/NavBar';
+import 'boxicons';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { CartContextProvider } from './context/CartContext';
 import Cart from './components/cart/Cart';
-import { NotificationProvider } from './notification/Notification';
-import 'boxicons';
-
 
 
 
 
 export const Context = createContext ()
 
-
+function App() {
+  const [cart, setCart] = useState([])
 
   return (
     <div className="App">
-        <NotificationProvider>
+       
         <CartContextProvider>
           <BrowserRouter>
             <NavBar />
@@ -33,7 +32,7 @@ export const Context = createContext ()
             </Routes>
           </BrowserRouter>
           </CartContextProvider>
-          </NotificationProvider>
+        
       </div>
   );
 }
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,25 +1,11 @@
-import { useContext } from "react"
-import CartContext from "../../context/CartContext"
+import ItemCart from "../ItemCart/ItemCart"
 
 const Cart = () => {
 
-    const { Cart, removeItem } = useContext(CartContext)
-
-    if(Cart.length === 0) {
-        return (
-            <h1>No hay productos</h1>
-        )
-    }
-
     return (
-        <>
-        <h1>Cart</h1>
-        <ul>
-            {
-                Cart.map(prod => <li key={prod.id}>{prod.name}  cantidad: {prod.quantity} precio uni: {prod.price}  subtotal: {prod.quantity * prod.price} <button onClick={() => removeItem(prod.id)}>X</button></li>)
-            }   
-        </ul>
-        </>
+        <div>
+            <ItemCart/>
+        </div>
     )
 }
 
